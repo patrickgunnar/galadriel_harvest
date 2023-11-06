3ce50b53b7f19c543ce4562d00882c3433f23130
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-11-06 19:34:36 -0300

    feat: added read file handler

--- index.js
+++ index.js
@@ -252,6 +252,6 @@ if (!nativeBinding) {
   throw new Error(`Failed to load native binding`)
 }
 
-const { sum } = nativeBinding
+const { processContent } = nativeBinding
 
-module.exports.sum = sum
+module.exports.processContent = processContent
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,31 @@
 #![deny(clippy::all)]
 
+use napi::Result;
+use std::io::Read;
+use std::{
+  fs::File,
+  path::Path
+};
+
 #[macro_use]
 extern crate napi_derive;
 
 #[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
+pub fn process_content(path: String) -> Result<()> {
+  // checks if the file exists
+  if Path::new(&path).exists() {
+    // attempt to open the file
+    let mut file = File::open(&path)?;
+    // Create a mutable string to store the file content
+    let mut file_content = String::new();
+
+    // Read the file content into the string
+    file.read_to_string(&mut file_content)?;
+
+    if !file_content.is_empty() {
+      println!("{}", file_content);
+    }
+  }
+
+  Ok(())
 }
